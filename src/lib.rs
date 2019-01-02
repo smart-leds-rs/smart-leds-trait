42c8d1ad294a13a3861a3b4f77511ccb11cbeb11
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-01-02 01:43:44 +0100

    Add `From` implementation for u8 tuples

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,15 @@ pub struct Color {
     pub b: u8,
 }
 
+impl From<(u8, u8, u8)> for Color {
+    fn from(color_tuple: (u8, u8, u8)) -> Self {
+        Self {
+            r: color_tuple.0,
+            g: color_tuple.1,
+            b: color_tuple.2,
+        }
+    }
+}
 pub trait SmartLedsWrite {
     type Error;
     fn write<T>(&mut self, iterator: T) -> Result<(), Self::Error>
