9a858ba829ef52ef3f040b3b9c847d5b0bc4cd43
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-03-24 01:06:05 +0100

    Use RGB8 type from rgb crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,7 @@
 #![no_std]
 
-#[derive(Copy, Clone, Default)]
-pub struct Color {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
-}
+pub type Color = rgb::RGB8;
 
-impl From<(u8, u8, u8)> for Color {
-    fn from(color_tuple: (u8, u8, u8)) -> Self {
-        Self {
-            r: color_tuple.0,
-            g: color_tuple.1,
-            b: color_tuple.2,
-        }
-    }
-}
 pub trait SmartLedsWrite {
     type Error;
     fn write<T>(&mut self, iterator: T) -> Result<(), Self::Error>
