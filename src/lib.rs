51302a37ee70b6b83c73f1e924d716ec8a04f5fb
David Sawatzke <d-git@sawatzke.dev>
2020-02-26 22:18:49 +0100

    Add some (much needed) documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,31 @@
+//! # Smart Leds Trait
+//!
+//! Smart leds is a collection of crates to use smart leds on embedded devices with rust.
+//!
+//! Examples of smart leds include the popular WS2812 (also called Neopixel),
+//! APA102 (DotStar) and other leds, which can be individually adressed.
+//!
+//! This crate is used as a common base, so that breaking changes which would
+//! force all other crates to be updated, can be avoided.
+//!
+//! End users should use the [smart-leds](https://crates.io/crates/smart-leds)
+//! crate, which contains various convenience functions.
 #![no_std]
 
 pub use {rgb::RGB, rgb::RGB16, rgb::RGB8, rgb::RGBA};
 
 pub struct White<C>(pub C);
 
+/// The RGBW Pixel
+///
+/// This is used for leds, that in addition to RGB leds also contain a white led
 pub type RGBW<ComponentType, WhiteComponentType = ComponentType> =
     RGBA<ComponentType, White<WhiteComponentType>>;
 
+/// A trait that smart led drivers implement
+///
+/// The amount of time each iteration of `iterator` might take is undefined.
+/// Drivers, where this might lead to issues, aren't expected to work in all cases.
 pub trait SmartLedsWrite {
     type Error;
     type Color;
